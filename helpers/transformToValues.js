9a0a677988fc4ecd8bc6f108721946ee0d407f45
Borna Pavlić <bpavlic@tmforum.org>
2022-10-25 16:51:33 +0200

    feat: adds version and release info

--- helpers/transformToValues.js
+++ helpers/transformToValues.js
@@ -4,16 +4,18 @@ export const transformToValues = (data) => {
   return Object.values(data)
     .map(([currElement]) => {
       const elementKey = currElement.context.split(',')[0];
-      const { document_number } = currElement;
+      const { document_number, version_info, release_info } = currElement;
       const {
         api_description: { api_name, api_description },
       } = currElement;
 
       return {
-        apiName: `${document_number} ${api_name}`,
+        apiName: api_name,
         apiDescription: removeHtmlTags(api_description),
         key: elementKey,
-        document_number,
+        documentNumber: document_number,
+        versionInfo: version_info,
+        releaseInfo: release_info,
       };
     })
     .reduce((prev, curr) => {
