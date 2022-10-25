f9b8cda04da372c200001c0fc1a179b1b125259e
Borna Pavlić <bpavlic@tmforum.org>
2022-10-25 16:48:16 +0200

    feat: adds redux

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,15 @@
 import Layout from '../components/Layout';
 import '../styles/global.scss';
+import { Provider } from 'react-redux';
+import { store } from '../redux/store/store';
+
 function MyApp({ Component, pageProps }) {
   return (
-    <>
+    <Provider store={store}>
       <Layout>
         <Component {...pageProps} />
       </Layout>
-    </>
+    </Provider>
   );
 }
 
