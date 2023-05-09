1be1351d3b942d9b6321fbd95befd3e811388d3b
Borna Pavlić <bpavlic@tmforum.org>
2023-05-09 15:41:23 +0200

    fix: removes env

--- services/userInfo/api.js
+++ services/userInfo/api.js
@@ -16,10 +16,13 @@ export const fetchUserInfo = async (token) => {
 export const fetchUserInfo2 = async (tmfUser) => {
   console.log(`${process.env.BASE_URL}/api/user_info`);
   try {
-    const res = await fetch(`${process.env.BASE_URL}/api/user_info`, {
-      method: 'POST',
-      body: tmfUser,
-    });
+    const res = await fetch(
+      `https://devbeta.ipsphere.net/oda/open-apis/table/api/user_info`,
+      {
+        method: 'POST',
+        body: tmfUser,
+      }
+    );
     const data = await res.json();
     return data;
   } catch (error) {
