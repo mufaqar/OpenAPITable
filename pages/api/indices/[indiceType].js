99388883b5682af5d64c8625921b8c04f824df54
Henrique Rodrigues <hrodrigues@tmforum.org>
2023-05-31 23:08:26 +0100

    revert AWS issue

--- pages/api/indices/[indiceType].js
+++ pages/api/indices/[indiceType].js
@@ -5,28 +5,30 @@ export default async function handler(req, res) {
   try {
     const response = await axios({
       method: 'get',
-      url: `https://tmf-open-api-table-documents.s3.eu-west-1.amazonaws.com/Indexes/index.json`,
+      url: `https://api.github.com/repos/tmforum-rand/api_table_docs/contents/index.json`,
       headers: {
+        Authorization: `Bearer ${process.env.GITHUB_TOKEN}`,
         'Content-Type': 'application/json',
       },
     });
-    res.status(200).json(response.data.content);
+    let data = process_github_index(indiceType, response.data.content);
+    res.status(200).json(data);
   } catch (err) {
     res.status(500).json(err);
   }
 }
 
-// function process_github_index(table, index) {
-//   let index_json = JSON.loads(index);
-//   let table_index = index_json[table];
-//   let flat_index = [];
-//   Object.values(table_index).forEach((api) => {
-//     api.forEach((entry) => {
-//       let description = entry.api_description;
-//       delete entry.api_description;
-//       flat_index.push({ ...description, ...entry });
-//     });
-//   });
+function process_github_index(table, index) {
+  let index_json = JSON.parse(Buffer.from(index, 'base64').toString('ascii'));
+  let table_index = index_json[table];
+  let flat_index = [];
+  Object.values(table_index).forEach((api) => {
+    api.forEach((entry) => {
+      let description = entry.api_description;
+      delete entry.api_description;
+      flat_index.push({ ...description, ...entry });
+    });
+  });
 
-//   return flat_index;
-// }
+  return flat_index;
+}
