9aa2dc733de39bd1902295798b754eb7a680a881
Borna Pavlić <bpavlic@tmforum.org>
2023-06-01 14:35:02 +0200

    fix: env value

--- pages/_app.js
+++ pages/_app.js
@@ -84,7 +84,7 @@ function MyApp({ Component, pageProps }) {
                 'strategic program': 'Open API Program',
                 program: 'API Project',
                 topic: 'API',
-                app: 'Open API Table - ${process.env.NEXT_PUBLIC_MARKETO_FORM_ID}',
+                app: 'Open API Table - ${process.env.NEXT_PUBLIC_ENVIRONMENT}',
                 permalink: url,
                 Email: email
                 });
