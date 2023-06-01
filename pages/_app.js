5a9678d8d80e5128548d92c0f1467aee5a0200a9
Borna Pavlić <bpavlic@tmforum.org>
2023-06-01 14:33:24 +0200

    fix: removes unused files

--- pages/_app.js
+++ pages/_app.js
@@ -84,7 +84,7 @@ function MyApp({ Component, pageProps }) {
                 'strategic program': 'Open API Program',
                 program: 'API Project',
                 topic: 'API',
-                app: 'Open API Table - Staging',
+                app: 'Open API Table - ${process.env.NEXT_PUBLIC_MARKETO_FORM_ID}',
                 permalink: url,
                 Email: email
                 });
