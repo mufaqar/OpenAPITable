325de93db4a8d2ff46a00a9694c251defac285c6
Borna Pavlić <bpavlic@tmforum.org>
2023-06-01 17:43:00 +0200

    feat: adds env

--- pages/api/user_info.js
+++ pages/api/user_info.js
@@ -17,13 +17,13 @@ export default async function handler(req, res) {
     })
       .then((r) => {
         res.setHeader('Access-Control-Allow-Credentials', true);
-        res.setHeader('Access-Control-Allow-Origin', '*');
+        res.setHeader(
+          'Access-Control-Allow-Origin',
+          `https://www.${process.env.NEXT_PUBLIC_HOST_DOMAIN}, https://${process.env.NEXT_PUBLIC_HOST_DOMAIN}`
+        );
         // another common pattern
         // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
-        res.setHeader(
-          'Access-Control-Allow-Methods',
-          'GET,OPTIONS,PATCH,DELETE,POST,PUT'
-        );
+        res.setHeader('Access-Control-Allow-Methods', 'POST');
         res.setHeader(
           'Access-Control-Allow-Headers',
           'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
