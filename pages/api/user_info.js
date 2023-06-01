18aeb254a1e3bb3e6c9602cfd157cb04054580a5
Borna Pavlić <bpavlic@tmforum.org>
2023-06-01 17:40:15 +0200

    feat: adds headers

--- pages/api/user_info.js
+++ pages/api/user_info.js
@@ -16,6 +16,18 @@ export default async function handler(req, res) {
       },
     })
       .then((r) => {
+        res.setHeader('Access-Control-Allow-Credentials', true);
+        res.setHeader('Access-Control-Allow-Origin', '*');
+        // another common pattern
+        // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+        res.setHeader(
+          'Access-Control-Allow-Methods',
+          'GET,OPTIONS,PATCH,DELETE,POST,PUT'
+        );
+        res.setHeader(
+          'Access-Control-Allow-Headers',
+          'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+        );
         res.status(200).json(r.data);
       })
       .catch((err) => {
