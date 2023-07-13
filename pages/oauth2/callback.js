1be2f5ebeaecba41e66946a2398c3344817ea9d3
Henrique Rodrigues <hrodrigues@tmforum.org>
2023-07-13 15:11:24 -0300

    callback with timeout

--- pages/oauth2/callback.js
+++ pages/oauth2/callback.js
@@ -19,7 +19,12 @@ const Callback = () => {
     if (auth.isAuthenticated) {
       localStorage.setItem('tmfUser', JSON.stringify(auth.user));
     }
-    router.push(currentPage || '/');
+    setTimeout(() => {
+      window.location.href =
+        currentPage ||
+        process.env.NEXT_PUBLIC_HOST_DOMAIN ||
+        'https://www.tmforum.org/';
+    }, 1);
   }, [auth, router]);
 
   return (
@@ -42,7 +47,14 @@ const Callback = () => {
       </p>
       <p className={gothamFont.className}>
         If you are not redirected, click{' '}
-        <Link className="redirect-link" href={'/'}>
+        <Link
+          className="redirect-link"
+          href={
+            pageUrl ||
+            process.env.NEXT_PUBLIC_HOST_DOMAIN ||
+            'https://www.tmforum.org/'
+          }
+        >
           here
         </Link>
         .
