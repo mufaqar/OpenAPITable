7b9bba9ef9b04fbae29e4105cfd3a911bf9f4add
alizaeda <alizaeda92@gmail.com>
2024-04-16 12:32:42 +0200

    feat: dynamic hero

--- components/organisms/Hero/Hero.tsx
+++ components/organisms/Hero/Hero.tsx
@@ -1,30 +1,38 @@
-import { Breadcrumb } from "@/components/atoms";
+import { Breadcrumb, BreadcrumbItem } from "@/components/atoms";
 import { HeroNavigation } from "@/components/molecules";
 import Image from "next/image";
 
-export const Hero = () => {
+interface IHeroProps {
+  title: string;
+  imageSrc?: string;
+  withNavigation?: boolean;
+  breadcrumbItems: BreadcrumbItem[];
+}
+
+export const Hero = ({
+  title,
+  imageSrc,
+  withNavigation,
+  breadcrumbItems,
+}: IHeroProps) => {
   return (
     <section className="py-6 bg-gradient-blue lg:rounded-bl-[160px] mt-[62px]">
       <div className="container">
         <div className="pb-6">
-          <Breadcrumb activeItem="Camara" />
+          <Breadcrumb items={breadcrumbItems} />
         </div>
 
         <div className="flex items-center justify-between mb-8 flex-wrap max-lg:space-y-10">
-          <h1 className="text-white text-5xl lg:text-hero font-bold">
-            GSMA Open Gateway/ <br />
-            CAMARA
+          <h1 className="text-white text-5xl lg:text-hero font-bold max-w-[780px]">
+            {title}
           </h1>
 
-          <Image
-            src="/oda/open-apis/images/partner-logo.png"
-            width={284}
-            height={70}
-            alt="Vodafone Logo"
-          />
+          {imageSrc && (
+            <Image src={imageSrc} width={284} height={70} alt="Vodafone Logo" />
+          )}
         </div>
 
-        <HeroNavigation />
+        {withNavigation ?? <HeroNavigation />}
       </div>
     </section>
   );
