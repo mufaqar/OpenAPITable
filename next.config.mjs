944cbdb8ef95472649d4432d602354a42ba7057b
mufaqar <mufaqar@gmail.com>
2024-04-17 17:07:28 +0500

    updated

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    missingSuspenseWithCSRBailout: false,
+  },
   basePath: "/oda/open-apis",
   async redirects() {
     return [
