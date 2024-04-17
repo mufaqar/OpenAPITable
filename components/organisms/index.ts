b190825b681e4458ad2822844d0081ac305b10dd
mufaqar <mufaqar@gmail.com>
2024-04-17 16:37:59 +0500

    fixing

--- components/organisms/index.ts
+++ components/organisms/index.ts
@@ -5,7 +5,7 @@ export * from "./Hero";
 export * from "./MainLayout";
 export * from "./Navigation";
 export { default as APIOverview } from "./OverviewSection/APIOverview"
-export { default as OverviewSection } from './OverviewSection/OverviewSection'
+//export { default as OverviewSection } from './OverviewSection/OverviewSection'
 export { default as APITable } from './APITable/APITable'
 export { default as IntroductionAPI } from './OverviewSection/IntroductionAPI'
 export { default as ContextSpecificBundles } from './ContextSpecificBundles/ContextSpecificBundles'
--- components/templates/Partners/Partners.tsx
+++ components/templates/Partners/Partners.tsx
@@ -2,13 +2,14 @@ import {
   AssetSection,
   FAQSection,
   Hero,
-  OverviewSection,
+
 } from "@/components/organisms";
+import { OverviewSection } from "@/components/organisms/OverviewSection/OverviewSection";
 
 export const Partners = () => {
   return (
     <>
-      <Hero />
+      <Hero title="" />
       <OverviewSection />
       <AssetSection />
       <FAQSection />
