3c11bfa0bfe523b8db80fe6aeef230205fc8be7e
Mufaqar <mufaqar@gmail.com>
2024-04-17 17:37:31 +0500

    update

--- components/organisms/SearchAPI/SearchAPI.tsx
+++ components/organisms/SearchAPI/SearchAPI.tsx
@@ -13,18 +13,18 @@ const SearchAPI = () => {
 
     return (
         <section className='bg-light-grey pb-5 z-10'>
-            <div className='container w-full mx-auto flex gap-5 md:pl-[20%]'>
-                <div className='bg-white border flex -mt-8 flex-1 gap-1 items-center p-1 px-5 rounded-tl-[20px] rounded-br-[20px]'>
+            <div className='container w-full mx-auto flex gap-3 md:gap-5 md:pl-60'>
+                <div className='bg-white border flex -mt-8 flex-1 gap-1 items-center p-1 px-3 md:px-5 rounded-tl-[20px] rounded-br-[20px]'>
                     <IoSearch size={20} className='text-gray-400' />
-                    <input type="text" placeholder='Find an API' className='w-full bg-white outline-none border-none py-3 px-2' />
+                    <input type="text" placeholder='Find an API' className='w-full bg-white outline-none border-none py-2 md:py-3 px-2' />
                 </div>
-                <div className={`bg-white border -mt-8 flex gap-3 items-center p-1 px-5 rounded-tl-[20px] rounded-br-[20px] text-gray-400 `}>
+                <div className={`bg-white border -mt-8 flex gap-3 items-center p-1 px-3 md:px-5 rounded-tl-[20px] rounded-br-[20px] text-gray-400 `}>
                     <button onClick={()=>handleTableStyle('grid')} className={`${tableStyle === 'grid' && 'text-core-blue'}`}>
-                        <TbDialpad size={26} />
+                        <TbDialpad className='text-[20px] md:text-[26px]' />
                     </button>
                     <div className='pr-[1.5px] h-[60%] w-full bg-gray-300'/>
                     <button onClick={()=>handleTableStyle('list')} className={`${tableStyle === 'list' && 'text-core-blue'}`}>
-                        <PiListDashesBold size={28} />
+                        <PiListDashesBold className='text-[22px] md:text-[28px]' />
                     </button>
                 </div>
             </div>
--- components/templates/OpenAPITable/index.tsx
+++ components/templates/OpenAPITable/index.tsx
@@ -1,5 +1,5 @@
 'use client'
-import React from 'react'
+import React, { Suspense } from 'react'
 import OpenAPITableGrid from './OpenAPITableGrid'
 import useSelectApiTableStyle from '@/hooks/useSelectApiTableStyle'
 import OpenAPITableList from './OpenAPITableList'
@@ -13,9 +13,12 @@ const OpenAPITable = () => {
       <Hero
         title="Open API Table"
       />
-      <SearchAPI />
-      { tableStyle === 'grid' ? <OpenAPITableGrid /> : <OpenAPITableList /> }
-
+      <Suspense><SearchAPI /></Suspense>
+      <section className='bg-light-grey'>
+        <div className='container pb-20 pt-5'>
+          {tableStyle === 'grid' ? <OpenAPITableGrid /> : <OpenAPITableList />}
+        </div>
+      </section>
     </>
   )
 }
