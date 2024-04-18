7dd8babb5fee3995e4c3f146a30a300cd4f214e9
Mufaqar <mufaqar@gmail.com>
2024-04-18 19:32:19 +0500

    remove any

--- components/organisms/BroadbandScenarios/BroadbandScenarios.tsx
+++ components/organisms/BroadbandScenarios/BroadbandScenarios.tsx
@@ -1,6 +1,12 @@
 import { BroadbandScenariesCard } from '@/components/molecules'
 import React from 'react'
 
+interface IBundle {
+    name: string;
+    info: string;
+  }
+
+
 export const BroadbandScenarios = () => {
     return (
         <section className='py-20 bg-core-blue'>
@@ -9,7 +15,7 @@ export const BroadbandScenarios = () => {
                 <p className='text-white mt-4 max-w-[800px]'>Introducing what scenarios are. Lorem ipsum dolor sit amet lacinia feugiat tempor. Tellus curabitur odio dictumst scelerisque sollicitudin rhoncus sapien.Lorem ipsum dolor sit amet lacinia feugiat tempor. Tellus curabitur odio dictumst scelerisque sollicitudin</p>
                 <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-10">
                     {
-                        bundles.map((item:any, idx:number) => (
+                        bundles.map((item:IBundle, idx:number) => (
                             <BroadbandScenariesCard data={item} key={idx}/>
                         ))
                     }
@@ -20,7 +26,7 @@ export const BroadbandScenarios = () => {
 }
 
 
-const bundles = [
+const bundles:IBundle[] = [
     {
         name: "Wholesale broadband",
         info: "Lorem ipsum dolor sit amet consectetur. Quis id sed imperdiet tempus netus enim morbi. Elementum fame",
