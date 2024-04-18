daf1d512bad9a6a90d91e75ddd37a71f78183059
Mufaqar <mufaqar@gmail.com>
2024-04-18 14:01:16 +0500

    update

---
+++ components/organisms/APITable/ApiGridCard.tsx
@@ -0,0 +1,36 @@
+import { IOpenAPITableData } from '@/util/types/ApiTable'
+import React, { FC } from 'react'
+import webHookLogoGreen from '../../../public/icons/WebhooksLogoGreen.svg'
+import Image from 'next/image'
+
+import { IoIosArrowForward } from "react-icons/io";
+import Link from 'next/link';
+
+
+interface IDataProps {
+    data: IOpenAPITableData
+}
+
+const ApiGridCard: FC<IDataProps> = ({ data }) => {
+    return (
+        <div className='bg-white px-2 py-3 flex gap-3 items-center justify-between rounded-xl flex-1'>
+            <div className='flex items-center gap-3'>
+                <figure className='min-w-[40px]'>
+                    <Image src={webHookLogoGreen} alt={data.title} width={40} height={40} />
+                </figure>
+                <div>
+                    <h4 className='capitalize text-sm lg:text-base'>{data.title}</h4>
+                    <ul className='flex gap-2'>
+                        <li className='text-[11px] rounded-br-[4px] px-1 py-[2px] border border-core-blue rounded-tl-[4px]'>{data.tag}</li>
+                        <li className='text-[11px] rounded-br-[4px] px-1 py-[2px] border border-core-blue rounded-tl-[4px]'>{data.version}</li>
+                    </ul>
+                </div>
+            </div>
+            <Link href="#">
+                <IoIosArrowForward className='text-[22px]' />
+            </Link>
+        </div>
+    )
+}
+
+export default ApiGridCard
--- components/templates/OpenAPITable/index.tsx
+++ components/templates/OpenAPITable/index.tsx
@@ -15,8 +15,14 @@ const OpenAPITable = () => {
       />
       <SearchAPI />
       <section className='bg-light-grey'>
-        <div className='container pb-20 pt-5'>
-          {tableStyle === 'grid' ? <OpenAPITableGrid /> : <OpenAPITableList />}
+        <div className='max-w-[1512px] mx-auto px-4 xl:px-6 2xl:px-8 w-full pb-20 pt-5'>
+          {tableStyle === 'grid' ?
+            <OpenAPITableGrid
+              MarketingSalesAPIs={MarketingSalesAPIs}
+              CustomerAPIs={CustomerAPIs}
+              ProductAPIs={ProductAPIs}
+            /> :
+            <OpenAPITableList />}
         </div>
       </section>
     </>
@@ -24,3 +30,118 @@ const OpenAPITable = () => {
 }
 
 export default OpenAPITable
+
+
+const MarketingSalesAPIs = [
+  {
+    title: 'AI Management',
+    version: 'V4.0.0',
+    domain: 'Resource APIs',
+    updated: '01/01/2023',
+    tag: 'TMF915'
+  },
+]
+
+const CustomerAPIs = [
+  {
+    title: 'AI Management',
+    version: 'V4.0.0',
+    domain: 'Resource APIs',
+    updated: '01/01/2023',
+    tag: 'TMF915'
+  },
+  {
+    title: 'AI Management',
+    version: 'V4.0.0',
+    domain: 'Resource APIs',
+    updated: '01/01/2023',
+    tag: 'TMF915'
+  },
+  {
+    title: 'AI Management',
+    version: 'V4.0.0',
+    domain: 'Resource APIs',
+    updated: '01/01/2023',
+    tag: 'TMF915'
+  },
+  {
+    title: 'AI Management',
+    version: 'V4.0.0',
+    domain: 'Resource APIs',
+    updated: '01/01/2023',
+    tag: 'TMF915'
+  },
+  {
+    title: 'AI Management',
+    version: 'V4.0.0',
+    domain: 'Resource APIs',
+    updated: '01/01/2023',
+    tag: 'TMF915'
+  },
+  {
+    title: 'AI Management',
+    version: 'V4.0.0',
+    domain: 'Resource APIs',
+    updated: '01/01/2023',
+    tag: 'TMF915'
+  },
+  {
+    title: 'AI Management',
+    version: 'V4.0.0',
+    domain: 'Resource APIs',
+    updated: '01/01/2023',
+    tag: 'TMF915'
+  },
+  {
+    title: 'AI Management',
+    version: 'V4.0.0',
+    domain: 'Resource APIs',
+    updated: '01/01/2023',
+    tag: 'TMF915'
+  },
+  {
+    title: 'AI Management',
+    version: 'V4.0.0',
+    domain: 'Resource APIs',
+    updated: '01/01/2023',
+    tag: 'TMF915'
+  },
+  {
+    title: 'AI Management',
+    version: 'V4.0.0',
+    domain: 'Resource APIs',
+    updated: '01/01/2023',
+    tag: 'TMF915'
+  },
+  {
+    title: 'AI Management',
+    version: 'V4.0.0',
+    domain: 'Resource APIs',
+    updated: '01/01/2023',
+    tag: 'TMF915'
+  }
+]
+
+const ProductAPIs = [
+  {
+    title: 'AI Management',
+    version: 'V4.0.0',
+    domain: 'Resource APIs',
+    updated: '01/01/2023',
+    tag: 'TMF915'
+  },
+  {
+    title: 'AI Management',
+    version: 'V4.0.0',
+    domain: 'Resource APIs',
+    updated: '01/01/2023',
+    tag: 'TMF915'
+  },
+  {
+    title: 'AI Management',
+    version: 'V4.0.0',
+    domain: 'Resource APIs',
+    updated: '01/01/2023',
+    tag: 'TMF915'
+  }
+]
