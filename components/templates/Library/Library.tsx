f0afa59edda9c43ec5ec853854554f25f6287bff
mostafazaied <zaiedmostafa@gmail.com>
2024-04-23 20:59:40 +0200

    add AssetSection to library page

--- components/templates/Library/Library.tsx
+++ components/templates/Library/Library.tsx
@@ -5,12 +5,13 @@ import { TableFilter } from "@/components/molecules";
 import {
   Hero,
   OverviewSection,
-  CodeReferenceSection,
   VersionHistorySection,
   ContextSection,
   RecentCertificationsSection,
+  AssetSection,
 } from "@/components/organisms";
 import { parseAsString, useQueryState } from "next-usequerystate";
+import { AssetListType } from "@/util/types";
 
 export const Library = () => {
   const [searchValue, setSearchValue] = useState("");
@@ -35,7 +36,7 @@ export const Library = () => {
         list={apiSingleRrestItems}
         description="Provides a standardized mechanism for placing a product order with all the necessary order parameters. The API consists of a simple set of operations that interact with CRM/Order negotiation systems in a consistent manner. A product order is created based on a product offering that is defined in a catalog. The product offering identifies the product or set of products that are available to a customer and includes characteristics such as pricing, product options and market."
       />
-      <CodeReferenceSection />
+      <AssetSection page={"Library"} assets={assets} />
       <VersionHistorySection backgroundColor={"bg-light-grey"} />
       <ContextSection />
       <RecentCertificationsSection />
@@ -63,5 +64,37 @@ const apiSingleRrestItems = [
   {
     label: "Pub Date",
     value: "01 JUL 2021",
+  },
+];
+const assets: AssetListType[] = [
+  {
+    title: "OPEN aPI SPECIFICATION",
+    type: "swagger",
+    url: "https://tmf-open-api-table-documents.s3.eu-west-1.amazonaws.com/OpenApiTable/5.0.0/swagger/TMF931-Open_Gateway_Operate_API_-_Onboarding_and_Ordering-v5.0.0.oas.yaml",
+  },
+  {
+    title: "Postman Collection",
+    type: "postman",
+    url: "https://tmf-open-api-table-documents.s3.eu-west-1.amazonaws.com/OpenApiTable/5.0.0/postman/TMF931-Open_Gateway_Onboarding_and_Ordering-v5.0.0-postman.yaml",
+  },
+  {
+    title: "API user guides specification (RAND)",
+    type: "user",
+    url: "https://tmf-open-api-table-documents.s3.eu-west-1.amazonaws.com/OpenApiTable/5.0.0/user_guides/TMF931-Open_Gateway_Onboarding_and_Ordering_Component_Suite-v5.0.0.pdf",
+  },
+  {
+    title: "Sample implementation code",
+    type: "code",
+    url: "https://tmf-open-api-table-documents.s3.eu-west-1.amazonaws.com/OpenApiTable/5.0.0/ri/TMF931_Open_Gateway_Operate-RI-v5.0.0.zip",
+  },
+  {
+    title: "CTK",
+    type: "checkmark",
+    url: "https://tmf-open-api-table-documents.s3.eu-west-1.amazonaws.com/OpenApiTable/5.0.0/ctk/CTK-TMF931-v5.0.0.zip",
+  },
+  {
+    title: "Conformance Profile (RAND)",
+    type: "menu",
+    url: "https://tmf-open-api-table-documents.s3.eu-west-1.amazonaws.com/OpenApiTable/5.0.0/conformance/TMF931_Operate_conformance.pdf",
   },
 ];
