2aa720769b30eacf5b1768a4df93afc8c9b89d64
Ali Eslam <alizaeda92@gmail.com>
2024-04-25 15:07:00 +0200

    homepage query example

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
 import { OpenAI } from "@/components/templates/OpenAI/OpenAI";
+import { fetchDataWithQS } from "@/lib/api";
 
-export default function Home() {
+export default async function Home() {
+  const data = await fetchDataWithQS("main-page", { populate: "*" });
+
   return (
     <>
-      <OpenAI/>
+      <OpenAI />
     </>
-  )
+  );
 }
